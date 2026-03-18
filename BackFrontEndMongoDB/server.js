44857bd82f26200b52bf558e495c1e1e73e7590c
Pedrito <pedrito@ucc.mx>
2026-03-17 19:38:06 -0600

    Servidor con conexion a mongo Ok

--- BackFrontEndMongoDB/server.js
+++ BackFrontEndMongoDB/server.js
@@ -11,42 +11,55 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-/*CONEXION CON MONGODB*/
+/*CONEXION CON MONGODB
 mongoose.connect("mongodb+srv://jestudillo:jestudillo@cluster0.6m8fm.mongodb.net/?appName=Cluster0")
 .then(()=>console.log("MongoDB conectado al proyecto"))
 .catch(err => console.log(err));
+*/
+mongoose.connect("mongodb+srv://jestudillo:jestudillo@cluster0.6m8fm.mongodb.net/GrupoB?appName=Cluster0")
+.then(
+    ()=>console.log("MongoDB database service listo!")
+)
+.catch(
+    err => console.log(err)
+);
 
 
-app.get("/api/usuarios", (req, res)=>{
-   res.json(registros);
+app.get("/api/usuarios", async(req, res)=>{
+   const usuarios= await Usuario.find();
+   res.json(usuarios);
 });
 
-app.post("/api/usuarios", (req, res)=>{
-    const nuevo = {
-        id:idActual++,
-        nombre:req.body.nombre,
-        email:req.body.email,
-        genero:req.body.genero,
-        plataformas:req.body.plataformas
-    };
-    registros.push(nuevo);
-    res.json(nuevo);
+app.post("/api/usuarios", async (req, res)=>{
+    const nuevo = new Usuario(
+        {
+            nombre: req.body.nombre,
+            email: req.body.email,
+            genero: req.body.genero,
+            plataformas: req.body.plataformas
+        }
+    );
+    const guardado= await nuevo.save();
+    res.json(guardado);
 });
 
-app.put("/api/usuarios/:id", (req, res)=>{
-    const id=parseInt(req.params.id);
-    const usuario=registros.find(u => u.id==id);
+app.put("/api/usuarios/:id", async (req, res)=>{
+    
+    const usuarioActualizado = await Usuario.findByIdAndUpdate(
+        req.params.id,
 
-    if(!usuario){
-        return res.status(404).json({mensaje:"Usuario no encontrado"});
-    }
-
-    usuario.nombre=req.body.nombre;
-    usuario.email=req.body.email;
-    usuario.genero=req.body.genero;
-    usuario.plataformas=req.body.plataformas;
-
-    res.json(usuario);
+        {
+            nombre: req.body.nombre,
+            email: req.body.email,
+            genero: req.body.genero,
+            plataformas: req.body.plataformas
+        },
+        
+        {new:true}
+    );
+    
+    res.json(usuarioActualizado);
+    
 });
 
 app.listen(port, ()=>{
