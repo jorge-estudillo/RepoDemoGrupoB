9309999f8999fed894bd64466a952331b039f7f2
Pedrito <pedrito@ucc.mx>
2026-04-28 19:59:24 -0600

    Con httpOnly

--- Demo_React/DemoSesion/src/main.jsx
+++ Demo_React/DemoSesion/src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 //import App from './App.jsx'
 //import AppLocalStorage from './sesionLocalStorage/AppLocalStorage'
-import AppCookie from './cookies/AppCookie'
+//import AppCookie from './cookies/AppCookie'
+import AppCookieHttpOnly from './cookieHttpOnly/AppCookieHttpOnly'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppCookie />
+    <AppCookieHttpOnly />
   </StrictMode>,
 )
